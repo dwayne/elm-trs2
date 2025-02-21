d2815608221463ed8a81238281ab061d0fdf6152
Dwayne Crooks <me@dwaynecrooks.com>
2025-02-21 07:05:34 -0400

    Document Logic.Value

--- src/Logic/Value.elm
+++ src/Logic/Value.elm
@@ -1,6 +1,6 @@
 module Logic.Value exposing (Value(..), toString)
 
-{-| Values.
+{-| Represents all the values that can be associated with variables.
 
 @docs Value, toString
 
@@ -9,7 +9,18 @@ module Logic.Value exposing (Value(..), toString)
 import Logic.Variable as Variable exposing (Variable)
 
 
-{-| -}
+{-| It is polymorphic in `a` so that you can specify your own constants.
+
+    type alias ValueS =
+        Value String
+
+    tea : ValueS
+    tea =
+        Const "tea"
+
+The constant type can be as rich as you need it to be.
+
+-}
 type Value a
     = Const a
     | Var Variable
@@ -18,7 +29,25 @@ type Value a
     | Pair (Value a) (Value a)
 
 
-{-| -}
+{-| Convert to a representation that is useful for debugging and testing.
+
+    u : ValueS
+    u =
+        Var (Variable "u" 0)
+
+    r : ValueS
+    r =
+        ReifiedVar 9
+
+    toString identity tea == "tea"
+    toString identity u == "u.0"
+    toString identity r == "_9"
+    toString identity Null == "()"
+    toString identity (Pair tea u) == "(tea . u.0)"
+    toString identity (Pair tea (Pair u (Pair r Null))) == "(tea u.0 _9)"
+    toString identity (Pair (Pair tea u) (Pair r Null)) == "((tea . u.0) _9)"
+
+-}
 toString : (a -> String) -> Value a -> String
 toString =
     toStringHelper "("
